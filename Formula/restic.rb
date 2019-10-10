2bbe1267597e1c977a2e7775c703517d3f6865de
daviehh <25255906+daviehh@users.noreply.github.com>
2019-10-10 16:50:46 -0400

    Update restic.rb
    enable cgo for mdns support
    https://github.com/restic/restic/issues/2335

--- Formula/restic.rb
+++ Formula/restic.rb
@@ -5,19 +5,14 @@ class Restic < Formula
   sha256 "e22208e946ede07f56ef60c1c89de817b453967663ce4867628dff77761bd429"
   head "https://github.com/restic/restic.git"
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "9f9218c65c562b2eace35015cff1a2dafd5a59dc5973165f3b59d7165f7d0a6c" => :mojave
-    sha256 "1775322bbdcd094cd3cded9097b5e07588fdd4c40e447fd2db319e37e8f2704e" => :high_sierra
-    sha256 "450abee766d22cd546b03beac010cef7e9b268aa0f9a223519697e3660985805" => :sierra
-  end
 
   depends_on "go" => :build
 
   def install
     ENV["GOPATH"] = HOMEBREW_CACHE/"go_cache"
+    ENV["CGO_ENABLED"] = "1"
 
-    system "go", "run", "build.go"
+    system "go", "run", "build.go", "--enable-cgo"
 
     mkdir "completions"
     system "./restic", "generate", "--bash-completion", "completions/restic"
